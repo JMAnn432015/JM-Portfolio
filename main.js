8422b5a259d4159c1fa378ca428c44b0ffa9fffe
JMAnn432015 <jmannacera@gmail.com>
2020-06-29 12:51:18 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+
+app.set('view engine','ejs');
+
+app.use(express.static('public'));
+
+app.get('/',(req,res)=>{
+    res.render('index');
+});
+
+app.get('/about',(req,res)=>{
+    res.render('about');
+});
+
+app.get('/portfolio',(req,res)=>{
+    res.render('portfolio');
+});
+
+app.get('/blog',(req,res)=>{
+    res.render('blog');
+});
+
+app.get('/contact',(req,res)=>{
+    res.render('contact');
+});
+
+app.listen(8000,(err)=>{
+    if(err) throw err;
+    console.log('Listening at port 8000');
+});
